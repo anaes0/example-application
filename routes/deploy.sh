d8abe70c741f5c374f2e2c78b0daeb2efb3379bf
anaes0 <anaesvg@gmail.com>
2022-06-28 10:59:39 +0100

    added continous delivery

---
+++ routes/deploy.sh
@@ -0,0 +1,12 @@
+#!/usr/bin/env bash
+sudo apt update && sudo apt install nodejs npm
+# Install pm2
+sudo npm install -g pm2
+#stop any instance of your app
+pm2 stop example_app
+# change directory
+cd ExampleApplication/
+# install app dependencies
+npm install
+#start the app
+pm2 start ./bin/www --name example_app
